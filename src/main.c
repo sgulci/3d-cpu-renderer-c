a1e4a7699e6a0cf7c1ce7886fda91e023905b9d0
sgulci <sahingulci@gmail.com>
2025-11-20 23:50:53 +0100

    sdl delay func added

--- src/main.c
+++ src/main.c
@@ -79,7 +79,12 @@ vec2_t project (vec3_t point){
 
 void update(void){
 
-  while(!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
+  int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+  if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME){
+    SDL_Delay(time_to_wait);
+  }
+
+  // while(!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
 
 
   previous_frame_time = SDL_GetTicks();
